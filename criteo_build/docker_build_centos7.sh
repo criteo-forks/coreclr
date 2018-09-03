63e94dba6f127d2314732d42a68263e1f8351663
Corentin Pescheloche <c.pescheloche@criteo.com>
2018-09-03 19:19:13 +0200

    USe microsoft docker image to build coreclr

--- criteo_build/docker_build_centos7.sh
+++ criteo_build/docker_build_centos7.sh
@@ -1,20 +1,7 @@
 #!/bin/sh -xe
 
 # This script starts docker
-
-docker run --privileged --detach --tty --interactive --env "container=docker" \
-       --volume /sys/fs/cgroup:/sys/fs/cgroup \
-       --volume `pwd`:/coreclr:rw  \
-       centos:centos7 \
-       /usr/sbin/init
-
-DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-docker logs $DOCKER_CONTAINER_ID
-docker exec --tty --interactive $DOCKER_CONTAINER_ID \
-       /bin/bash -xec "bash -xe /coreclr/build.sh;
-       echo -ne \"------\nEND CORECLR build\n\";"
-
-docker ps -a
-docker stop $DOCKER_CONTAINER_ID
-docker rm -v $DOCKER_CONTAINER_ID
-
+ROOT=$(pwd)
+DOCKER_ARGS="run -i --rm -v ${ROOT}:/mnt/coreclr -w /mnt/coreclr  microsoft/dotnet-buildtools-prereqs:centos-7-b46d863-20180719033416"
+docker ${DOCKER_ARGS} /mnt/coreclr/build.sh
+docker ${DOCKER_ARGS} /mnt/coreclr/build-test.sh
